b92a06d34fd37f0695c05c302745687903e232df
Karl-Henrik Horve <karlhenh@stud.ntnu.no>
2023-02-08 13:15:06 +0100

    This should work

--- elkStack/elkStack_skyhigh_install.sh
+++ elkStack/elkStack_skyhigh_install.sh
@@ -9,10 +9,7 @@ gnupg \
 lsb-release \
 net-tools \
 
-# Pulling git repo and navigating to home
-cd /home/ubuntu/
-sudo git clone https://github.com/BenRedic-FyFazan/bookface.git
-
+## INSTALLATION
 # Installing https transport and java runtime environment
 sudo apt update
 sudo apt install apt-transport-https
@@ -36,19 +33,61 @@ echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://arti
 sudo apt-get update && sudo apt-get install elasticsearch
 sudo systemctl enable elasticsearch
 
-## Disabling for easier install
-# sudo systemctl start elasticsearch
+# Logstash
+sudo apt-get install logstash
+sudo systemctl enable logstash
+
+# Kibana
+sudo apt-get install kibana
+sudo systemctl enable kibana
+
+# Firebeat
+sudo apt-get install filebeat
+sudo systemctl enable firebeat
 
 ## DID DEPLOY CHECK WORK?! IT WORKS
 
-# configuring elkStack
+## CONFIGURATION
+
+# Pulling git repo and navigating to home
+cd /home/ubuntu/
+sudo git clone https://github.com/BenRedic-FyFazan/bookface.git
+
+# elkStack config
 cd ./bookface/elkStack/
 sudo rm /etc/elasticsearch/elasticsearch.yml
 sudo cp code/elasticSearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 # jvm_max size
 sudo cp code/elasticSearch/jvm_heap_size.options /etc/elasticsearch/jvm.options.d/jvm_heap_size.options
-## Disabling for easier install
-# sudo systemctl restart elasticsearch
 
-# Installing the rest
-./elkLogstash_install.sh
+# Logstash config
+## None atm
+
+# Kibana config
+cd /home/ubuntu/bookface/elkStack/
+sudo rm /etc/kibana/kibana.yml
+sudo cp code/kibana/kibana.yml /etc/kibana/kibana.yml
+
+# Filebeat config
+cd /home/ubuntu/bookface/elkStack/
+sudo rm /etc/filebeat/filebeat.yml
+sudo cp code/filebeat/filebeat.yml /etc/filebeat/filebeat.yml
+sudo filebeat modules enable system
+
+## loading filebeat index
+sudo filebeat setup --index-managementsudo filebeat setup \
+ --index-management -E output.logstash.enabled=false \
+-E 'output.elasticsearch.hosts=["0.0.0.0:9200"]'
+
+sudo systemctl enable filebeat
+## Disabling for easier install
+# sudo systemctl start elasticsearch
+# sudo systemctl start logstash
+# sudo systemctl start kibana
+# sudo systemctl start filebeat
+
+
+
+
+
+
