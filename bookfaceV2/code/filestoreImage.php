0fff0beb8a2d0cd2b7d746047f66694b6b4af593
Karl-Henrik Horve <karlhenh@stud.ntnu.no>
2023-03-22 13:57:57 +0100

    .

--- bookfaceV2/code/filestoreImage.php
+++ bookfaceV2/code/filestoreImage.php
@@ -1,5 +1,5 @@
 <?php
-
+$start_time = microtime(true);
 $image = $_GET['image'];
 include_once "config.php";
 
@@ -9,12 +9,16 @@ if ( $memcache_enabled == 1 and $memcache_enabled_pictures== 1) {
     $memcache->addServer($memcache_server, "11211");
 }
 
+$source = "filesystem"; // Default to filesystem
+
 if ($memcache) {
     $key = "picture_" . $image;
     $picture_of_user = $memcache->get($key);
 
     if ( !$picture_of_user) {
         $picture_of_user = file_get_contents(__DIR__.'/images/' . $image);
+    } else {
+        $source = "memcache";
     }
 
     if ($picture_of_user) {
@@ -27,7 +31,12 @@ if ($memcache) {
 } else {
     $picture_of_user = file_get_contents('/images/' . $image);
 }
+
+$execution_time = microtime(true) - $start_time;
+
 header('Content-Type: image/jpeg');
+header("X-Image-Source: $source");
+header("X-Execution-Time: {$execution_time}s");
 echo $picture_of_user;
 
 
